b7e23852316c2c5413776ad5582214e1764f5107
evoskuil <eric@voskuil.org>
2015-03-19 16:26:09 -0700

    Update autogen generation.

--- templates/gsl.autogen.sh
+++ templates/gsl.autogen.sh
@@ -7,35 +7,22 @@
 # This is a code generator built using the iMatix GSL code generation
 # language. See https://github.com/imatix/gsl for details.
 ###############################################################################
-# Macros
+# Generation
 ###############################################################################
-.endtemplate
-.template 1
-.
-.macro autogen_content()
+.macro generate_autogen()
+.for generate.repository by name as _repository
+.    define my.out_file = "$(_repository.name)/autogen.sh"
+.    notify(my.out_file)
+.    output(my.out_file)
+.    
+.    shebang("bash")
+.    copyleft(_repository.name)
 
 autoreconf -i
 .
-.endmacro # autogen_content
-.
-.endtemplate
-.template 0
-###############################################################################
-# Generation
-###############################################################################
-function generate_autogen()
-for generate.repository by name as _repository
-    define my.out_file = "$(_repository.name)/autogen.sh"
-    notify(my.out_file)
-    output(my.out_file)
-    
-    shebang("bash")
-    copyleft(_repository.name)
-    autogen_content()
-
-    close
-endfor _repository
-endfunction # generate_autogen
+.    close
+.endfor _repository
+.endmacro # generate_autogen
 .endtemplate
 
 
