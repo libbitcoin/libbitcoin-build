67aa3358006c16bf287790cef986520bd5a413e1
evoskuil <eric@voskuil.org>
2016-02-04 15:48:18 -0800

    Untrack local generated sources.

--- libbitcoin-network/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-###############################################################################
-#  Copyright (c) 2014-2015 libbitcoin-network developers (see COPYING).
-#
-#         GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
-#
-###############################################################################
-
-autoreconf -i
