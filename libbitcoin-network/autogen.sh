39c159078f829645d39f11369d261a44c1dbae62
evoskuil <eric@voskuil.org>
2016-02-03 18:21:03 -0800

    Generate libbitcoin-network, comment in dependencies on libbitcoin-network.

---
+++ libbitcoin-network/autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+###############################################################################
+#  Copyright (c) 2014-2015 libbitcoin-network developers (see COPYING).
+#
+#         GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
+#
+###############################################################################
+
+autoreconf -i
