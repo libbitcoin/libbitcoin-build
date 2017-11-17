d647334e2dd86dd8fc6a1f474bf6f92d0725d96f
Phillip Mienk <mienkphi@gmail.com>
2017-11-17 00:42:18 -0800

    Begin isolated template execution/generation.

---
+++ gsl_update_repository_artifacts.cmd
@@ -0,0 +1,71 @@
+.template 0
+###############################################################################
+# Copyright (c) 2014-2015 libbitcoin developers (see COPYING).
+#
+# GSL generate version.hpp.
+#
+# This is a code generator built using the iMatix GSL code generation
+# language. See https://github.com/imatix/gsl for details.
+###############################################################################
+[global].root = ".."
+[global].trace = 0
+[gsl].ignorecase = 0
+
+gsl from "utilities.gsl"
+
+.endtemplate
+.template 1
+.macro generate_artifact()
+.   define out_file = "update_repository_artifacts.cmd"
+.   notify(out_file)
+.   output(out_file)
+.   batch_no_echo()
+.   bat_copyleft("libbitcoin-build")
+
+REM Do everything relative to this file location.
+pushd %~dp0
+
+REM Clean directories for generated build artifacts.
+.   for buildgen->repositories.repository by name as _repo
+rmdir /s /q $(_repo.name) 2>NUL
+.   endfor
+
+REM Generate build artifacts.
+.   for buildgen->templates.template as _template
+gsl -q -script:wrappers\\wrapped_\$(_template.name).gsl generate.xml
+if %errorlevel% neq 0 goto error
+
+.   endfor
+
+REM Copy outputs to all repositories.
+.   for buildgen->repositories.repository by name as _repo
+xcopy /s /y $(_repo.name)\\* ..\\$(_repo.name)\\
+.   endfor
+
+REM Generate bindings from generated binding generators.
+REM The path to swig.exe must be in our path.
+.   for buildgen->repositories.repository by name as _repo
+REM call ..\\$(_repo.name)\\bindings.bat
+.   endfor
+
+echo --- OKAY, generation completed.
+goto end
+
+:error
+echo *** ERROR, generation terminated early.
+
+:end
+REM Restore directory.
+popd
+
+REM Delay for manual confirmation.
+pause
+
+.endmacro generate_artifact
+.endtemplate
+.template 0
+
+generate_artifact()
+
+.endtemplate
+###############################################################################
---
+++ gsl_update_repository_artifacts.sh
@@ -0,0 +1,94 @@
+.template 0
+###############################################################################
+# Copyright (c) 2014-2015 libbitcoin developers (see COPYING).
+#
+# GSL generate version.hpp.
+#
+# This is a code generator built using the iMatix GSL code generation
+# language. See https://github.com/imatix/gsl for details.
+###############################################################################
+[global].root = ".."
+[global].trace = 0
+[gsl].ignorecase = 0
+
+gsl from "utilities.gsl"
+
+.endtemplate
+.template 1
+.macro generate_artifact()
+.   define out_file = "update_repository_artifacts.sh"
+.   notify(out_file)
+.   output(out_file)
+.   shebang("bash")
+.   copyleft("libbitcoin-build")
+
+# Exit this script on the first build error.
+set -e
+
+declare -a project=( \\
+.   for buildgen->repositories.repository by name as _repo
+    "$(_repo.name)" \\
+.   endfor
+    )
+
+declare -a generator=( \\
+.   for buildgen->templates.template as _template
+    "$(_template.name)" \\
+.   endfor
+    )
+
+cleanup_local_files()
+{
+    for proj in "\${project[@]}"
+    do
+        eval rm -rf "\$proj"
+    done
+}
+
+generate_artifacts()
+{
+    for gen in "\${generator[@]}"
+    do
+        eval gsl -q "-script:wrappers/wrapped_\$gen.gsl" generate.xml
+    done
+}
+
+mark_executables()
+{
+    for proj in "\${project[@]}"
+    do
+        eval chmod +x "\$proj/*.sh"
+    done
+}
+
+overwrite_project_files()
+{
+    for proj in "\${project[@]}"
+    do
+        eval cp -rf "\$proj/." "../$\proj"
+    done
+}
+
+# Do everything relative to this file location.
+cd `dirname "$0"`
+
+# Clean directories for generated build artifacts.
+cleanup_local_files
+
+# Generate build artifacts.
+generate_artifacts
+
+# Make generated scripts executable.
+mark_executables
+
+# Copy outputs to all repositories.
+overwrite_project_files
+
+.endmacro generate_artifact
+.endtemplate
+.template 0
+
+generate_artifact()
+
+.endtemplate
+###############################################################################
