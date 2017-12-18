e6666c5e6712f0473a5f375afa29568721e141f3
Phillip Mienk <mienkphi@gmail.com>
2017-12-18 02:04:39 -0800

    Adds imports and props management for Visual Studio 2013/2015/2017.

--- gsl_update_repository_artifacts.sh
+++ gsl_update_repository_artifacts.sh
@@ -2,17 +2,14 @@
 ###############################################################################
 # Copyright (c) 2014-2015 libbitcoin developers (see COPYING).
 #
-# GSL generate version.hpp.
+# GSL generate update_repository_artifacts.sh.
 #
 # This is a code generator built using the iMatix GSL code generation
 # language. See https://github.com/imatix/gsl for details.
 ###############################################################################
-[global].root = ".."
-[global].trace = 0
-[gsl].ignorecase = 0
-
-gsl from "utilities.gsl"
-
+###############################################################################
+# Macros
+###############################################################################
 .endtemplate
 .template 1
 .macro generate_artifact()
@@ -37,11 +34,6 @@ declare -a generator=( \\
 .   endfor
     )
 
-cleanup_local_files()
-{
-    eval rm -rf "output"
-}
-
 generate_artifacts()
 {
     for gen in "\${generator[@]}"
@@ -53,26 +45,10 @@ generate_artifacts()
 mark_executables()
 {
     eval chmod +x "output/*/*.sh"
-#    for proj in "\${project[@]}"
-#    do
-#        eval chmod +x "output/\$proj/*.sh"
-#    done
-}
-
-overwrite_project_files()
-{
-    eval cp -rf "output/." "../"
-#    for proj in "\${project[@]}"
-#    do
-#        eval cp -rf "output\$proj/." "../$\proj"
-#    done
 }
 
 # Do everything relative to this file location.
 cd `dirname "$0"`
-
-# Clean directories for generated build artifacts.
-cleanup_local_files
 
 # Generate build artifacts.
 generate_artifacts
@@ -80,14 +56,22 @@ generate_artifacts
 # Make generated scripts executable.
 mark_executables
 
-# Copy outputs to all repositories.
-overwrite_project_files
-
 .endmacro generate_artifact
 .endtemplate
 .template 0
+###############################################################################
+# Execution
+###############################################################################
+[global].root = ".."
+[global].trace = 0
+[gsl].ignorecase = 0
+
+# Note: expected context root libbitcoin-build directory
+gsl from "library/math.gsl"
+gsl from "library/string.gsl"
+gsl from "library/collections.gsl"
+gsl from "utilities.gsl"
 
 generate_artifact()
 
 .endtemplate
-###############################################################################
--- update_codegen.sh
+++ update_codegen.sh
@@ -11,10 +11,43 @@
 # Exit this script on the first build error.
 set -e
 
-gsl -q -script:gsl_update_repository_artifacts.cmd buildgen.xml
-gsl -q -script:gsl_update_repository_artifacts.sh buildgen.xml
+cleanup_local_files()
+{
+    eval rm -rf "output"
+}
 
-eval chmod +x update_repository_artifacts.sh
+overwrite_project_files()
+{
+    eval cp -rf "output/." "../"
+}
 
-eval ./update_repository_artifacts.sh
+generate_updaters()
+{
+    eval gsl -q -script:gsl_update_repository_artifacts.cmd buildgen.xml
+    eval gsl -q -script:gsl_update_repository_artifacts.sh buildgen.xml
+    eval chmod +x update_repository_artifacts.sh
+    eval gsl -q -script:gsl_update_seeded_artifacts.cmd generate.xml
+    eval gsl -q -script:gsl_update_seeded_artifacts.sh generate.xml
+    eval chmod +x update_seeded_artifacts.sh
+}
 
+execute_updaters()
+{
+    eval ./update_seeded_artifacts.sh
+    eval ./update_repository_artifacts.sh
+}
+
+# Do everything relative to this file location.
+cd `dirname "$0"`
+
+# Clean directories for generated build artifacts.
+cleanup_local_files
+
+# Generate second-stage artifact generators.
+generate_updaters
+
+# Execute second-stage artifact generation.
+execute_updaters
+
+# Copy outputs to all repositories.
+#overwrite_project_files
