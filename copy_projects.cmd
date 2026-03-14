31ff6b1b1c3263d35b6fe5cb0bb1f748a03d0c1b
Phillip Mienk <mienkphi@gmail.com>
2026-03-13 21:07:40 -0700

    Rationalize copy_projects.cmd and generated file.

--- copy_projects.cmd
+++ copy_projects.cmd
@@ -7,42 +7,40 @@ set "dest_dir=.."
 
 :: Check if source directory exists
 if not exist "%source_dir%\" (
-    echo Error: Source directory "%source_dir%" does not exist.
+    echo Error: Source directory '%source_dir%' does not exist.
     exit /b 1
 )
 
-:: Create destination directory if it doesn't exist
+:: Check if destination directory exists
 if not exist "%dest_dir%\" (
-    echo Error: Destination directory "%dest_dir%" does not exits.
+    echo Error: Destination directory '%dest_dir%' does not exits.
     exit /b 1
 )
 
 :: If no substring arguments are provided, copy all subdirectories
+set copied=0
 if "%~1"=="" (
-    echo No substrings specified. Copying all subdirectories from "%source_dir%" to "%dest_dir%"...
-    for /d %%i in ("%source_dir%\*") do (
-        set "subdir=%%~nxi"
-        echo Copying '!subdir!' to '%dest_dir%'...
-        xcopy "%%i" "%dest_dir%\!subdir!\" /E /I /Y
+    echo No substrings specified. Copying 'libbitcoin' subdirectories from '%source_dir%' to '%dest_dir%'...
+
+    for /d %%i in ("%source_dir%\libbitcoin*") do (
+        echo Copying '%%~nxi' to '%dest_dir%'...
+        xcopy "%%i" "%dest_dir%\%%~nxi\" /E /I /Y
+        set /a copied+=1
     )
 ) else (
     :: Copy subdirectories whose names contain any of the provided substrings
-    set copied=0
     for /d %%i in ("%source_dir%\*") do (
-        set "subdir=%%~nxi"
-        set found=0
         for %%j in (%*) do (
-            echo.!subdir! | find /i "%%j" >nul
+            echo.%%~nxi | find /i "%%j" >nul
             if !errorlevel! equ 0 (
-                echo Copying '!subdir!' to '%dest_dir%'...
-                xcopy "%%i" "%dest_dir%\!subdir!\" /E /I /Y
-                set found=1
+                echo Copying '%%~nxi' to '%dest_dir%'...
+                xcopy "%%i" "%dest_dir%\%%~nxi\" /E /I /Y
                 set /a copied+=1
             )
         )
     )
     if !copied! equ 0 (
-        echo Warning: No subdirectories found in "%source_dir%" matching any provided substrings.
+        echo Warning: No subdirectories found in '%source_dir%' matching any provided substrings.
     )
 )
 
